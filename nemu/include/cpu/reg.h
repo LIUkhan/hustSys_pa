3856574afea6dc565055cf1a4e85655fff06c628
tracer-ics2018 <tracer@njuics.org>
2018-12-12 11:42:38 +0800

    >  compile U201514639 khan Linux khan 4.4.0-139-generic #165-Ubuntu SMP Wed Oct 24 10:58:50 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  11:42:38 up  3:38,  9 users,  load average: 0.27, 0.15, 0.14 dd0f6e4f599fa5f359b0658951a44949806d7c5

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -28,9 +28,7 @@ typedef struct {
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
-  union{
-    rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-  };
+  rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 
   vaddr_t eip;
 
