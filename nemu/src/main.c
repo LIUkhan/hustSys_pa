92860cea7418ee057cd4c40024acef32804b4800
tracer-ics2018 <tracer@njuics.org>
2018-12-17 11:46:14 +0800

    >  compile U201514639 khan Linux khan 4.4.0-139-generic #165-Ubuntu SMP Wed Oct 24 10:58:50 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  11:46:14 up  3:37,  1 user,  load average: 0.45, 0.42, 0.41 9de518b05105ea4254c634857ef0fc1383bbca3

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -5,7 +5,7 @@ int init_monitor(int, char *[]);
 void ui_mainloop(int);
 
 struct{
-  char result[100];
+  uint32_t result;
   char buf[65536];
 } test[100];
 extern uint32_t expr(char *, bool *);
@@ -13,21 +13,17 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
   bool success;
-  FILE *fptr = fopen("src/testinput","r");
-  if(fptr == NULL)
-    printf("1\n");
-  else
-    printf("2\n");
+  FILE *fptr = fopen("input","r");
   for(int i = 0; i < 100; i++)
   {
-    int sta1 = fscanf(fptr,"%s",test[i].result);
+    int sta1 = fscanf(fptr,"%u",&test[i].result);
     char* sta2 = fgets(test[i].buf,65536,fptr);
     printf("%d\n",sta1);
     if(sta1 == -1 || sta2 == NULL){
       printf("ERR\n");
       continue;
     }
-    printf("%u \n",expr(test[i].buf,&success));
+    printf("%u %u\n",expr(test[i].buf,&success),test[i].result);
   }
   fclose(fptr);
   /* Receive commands from user. */
