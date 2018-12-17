f386ce380349b2fd2e55a89b07f312cb426eca9a
tracer-ics2018 <tracer@njuics.org>
2018-12-17 16:38:25 +0800

    >  compile U201514639 khan Linux khan 4.4.0-139-generic #165-Ubuntu SMP Wed Oct 24 10:58:50 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:38:25 up  8:30,  1 user,  load average: 0.34, 0.19, 0.19 40c930a0c14b9411bacd1a91e15e14de21a7fb46

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -159,7 +159,7 @@ static bool make_token(char *e) {
       }
     }
 
-    if (i == NR_REGEX && e[position] != '\0') {
+    if (i == NR_REGEX && e[position] != '\n' && e[position] != '\0') {
       printf("\nattention:%d %d %d\n",e[position],e[position+3],e[position+4]);
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
