b169e2eb7f232f87f1efb145fdb914ae35f30d72
tracer-ics2018 <tracer@njuics.org>
2018-12-17 11:08:23 +0800

    >  compile U201514639 khan Linux khan 4.4.0-139-generic #165-Ubuntu SMP Wed Oct 24 10:58:50 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  11:08:23 up  3:00,  1 user,  load average: 0.70, 0.49, 0.39 2ffb4b4e7419ef88b8f8fce24f045ebd4cc9883b

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,12 +1,31 @@
+#include "nemu.h"
+#include <stdio.h>
+
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
 
+struct{
+  uint32_t result;
+  char buf[65536];
+} test[100];
+extern uint32_t expr(char *, bool *);
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
 
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
-
+  bool success;
+  FILE *fp = fopen("../tools/gen-expr/input", "r");
+  for(int i = 0; i < 100; i++)
+  {
+    int sta1 = fscanf(fp,"%u",&test[i].result);
+    char* sta2 = fgets(test[i].buf,65536,fp);
+    if(sta1 == 0 || sta2 == NULL){
+      printf("ERR\n");
+      continue;
+    }
+    printf("%u  %u\n",expr(test[i].buf,&success),test[i].result);
+  }
   return 0;
 }
