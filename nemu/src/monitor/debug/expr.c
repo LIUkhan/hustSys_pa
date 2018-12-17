245e53def56a16a209879e6136d3b3015bfb86c5
tracer-ics2018 <tracer@njuics.org>
2018-12-17 16:52:14 +0800

    >  compile U201514639 khan Linux khan 4.4.0-139-generic #165-Ubuntu SMP Wed Oct 24 10:58:50 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:52:14 up  8:43,  1 user,  load average: 0.28, 0.32, 0.27 dfdd740b41c2657bc5ae2a77ba862d369363c15

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -257,6 +257,10 @@ uint32_t eval(uint32_t p,uint32_t q)
 //检查满足BNF的括号表达式
 bool check_parentheses(uint32_t p,uint32_t q)
 {
+  while(tokens[p].type == TK_NOTYPE)
+    p++;
+  while(tokens[q].type == TK_NOTYPE)
+    q--;
   //不符合BNF的要求
   if(tokens[p].type != TK_LP || tokens[q].type != TK_RP)
     return false;
