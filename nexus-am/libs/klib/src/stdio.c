f74b24897b94dae5e19beafedcc59800bab483f5
tracer-ics2018 <tracer@njuics.org>
2018-12-26 22:48:59 +0800

    >  run U201514639 khan Linux khan 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  22:48:59 up 14:48,  1 user,  load average: 0.31, 0.28, 0.19 b5040385f93fdf3a83b86f5ebae2178144550fb9

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -11,7 +11,7 @@ typedef struct info{
   uint32_t step;
 }info;
 
-void recognize(char *out,int index,info *r)
+void recognize(const char *out,int index,info *r)
 {
   r->addzero = 0;
   r->width = 0;//0不限制长度
@@ -23,7 +23,7 @@ void recognize(char *out,int index,info *r)
     index++;
     r->step++;
   }
-  while(out[index] >= '1' && out[index] <= '9')
+  while(out[index] >= '0' && out[index] <= '9')
   {
     r->width += r->width *10 + out[index]-48;
     index++;  r->step++;
@@ -42,42 +42,57 @@ void _puts(char *str)
 }
 
 
-void i2a(int num,int nindex,char *out,int *index,info form)
+void uori2a(int num,int nindex,char *out,int *index,info form,int isunsigned)
 {
   int i = 0,len;
   char numbuf[2000];
-  int low = num%10;
-  if(num == 0)
-    numbuf[i++] = 48;
-  else{
-    while(num != 0)
-    {
-      numbuf[i++] = 48 + low;
-      num /= 10;
-      low = num%10;
-    }  
-  }
-  numbuf[i] = '\0';
-  len = strlen(numbuf);
-  if(len < form.width)
-  {
-    int subres = form.width - len;
-    if(form.addzero)
-      for(int j = 0; j < subres; j++)
-        numbuf[len+j] = '0';
-    else
-      for(int j = 0; j < subres; j++)
-        numbuf[len+j] = ' ';
-    numbuf[len+subres] = '\0';
-  }
-  if(out == NULL)
-  {
-    for(int j = len-1; j >= 0; j--)
-      _putc(numbuf[j]);
-    *index+=strlen(numbuf);
+  if(!isunsigned) {
+    int low = num%10;
+    if(num == 0)
+      numbuf[i++] = 48;
+    else{
+      while(num != 0)
+      {
+        numbuf[i++] = 48 + low;
+        num /= 10;
+        low = num%10;
+      }  
+    }
   }
   else
   {
+    uint32_t unum = (uint32_t)num;
+    uint32_t low = unum%10;
+    if(unum == 0)
+      numbuf[i++] = 48;
+    else{
+      while(unum != 0)
+      {
+        numbuf[i++] = 48 + low;
+        unum /= 10u;
+        low = unum%10u;
+      }  
+    }
+  }
+  numbuf[i] = '\0';
+  if(i < form.width)
+  {
+    int subres = form.width - i;
+    if(form.addzero)
+      for(int j = 0; j < subres; j++)
+        numbuf[i+j] = '0';
+    else
+      for(int j = 0; j < subres; j++)
+        numbuf[i+j] = ' ';
+    numbuf[i+subres] = '\0';
+  }
+  len = strlen(numbuf);
+  if(out == NULL) {
+    for(int j = len-1; j >= 0; j--)
+      _putc(numbuf[j]);
+    *index += len;
+  }
+  else {
     for(int j = len-1; j >= 0; j--)
       out[nindex++] = numbuf[j];
     out[nindex] = '\0';
@@ -104,7 +119,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
     if(fmt[i] == '%')
     {
       info form;
-      recognize(out,i+1,&form);
+      recognize(fmt,i+1,&form);
       switch(form.iden)
       {
         case 's':{
@@ -137,22 +152,29 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
         case 'd':{
           int num = va_arg(ap,int);
           int nindex = strlen(out);
-          i2a(num,nindex,out,&index,form);
+          uori2a(num,nindex,out,&index,form,0);
+          i+=form.step;
+          break;
+        }
+        case 'u':{
+          int num = va_arg(ap,int);//在函数内部转为ｕｉｎｔ
+          int nindex = strlen(out);
+          uori2a(num,nindex,out,&index,form,1);
           i+=form.step;
           break;
         }
         default:{
+          _putc('N');
+          _putc('\n');
           break;
         }
       }
     }
     if(out != NULL)
     {
-      index = strlen(out);
-      if(i >= len) {
-        out[index] = '\0';
+      if(i >= len)
         break;
-      }
+      index = strlen(out);
       out[index++] = fmt[i];
       out[index] = '\0';
     }
@@ -162,7 +184,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
       _putc(fmt[i]);
       index++;
     }
-  };
+  }
   return index;
 }
 
@@ -174,7 +196,6 @@ int sprintf(char *out, const char *fmt, ...) {
   va_start(ap,fmt);
   cnt = vsprintf(out,fmt,ap);
   _puts(out);
-  _putc('\n');
   va_end(ap);
   return cnt;
 }
