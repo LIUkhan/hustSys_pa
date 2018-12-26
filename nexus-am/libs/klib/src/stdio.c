01689b3b2aea66ddb8c14167db45d46c12279dff
tracer-ics2018 <tracer@njuics.org>
2018-12-26 11:10:54 +0800

    >  run U201514639 khan Linux khan 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  11:10:54 up  3:10,  1 user,  load average: 0.41, 0.38, 0.36 4dd38ab17301c5729310653010a4180c8084c57d

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -5,26 +5,46 @@
 
 void i2a(int num,int nindex,char *out)
 {
+  int i = 0,len;
+  char numbuf[2000];
   int low = num%10;
-  while(low != 0)
-  {
-    out[nindex++] = 48 + low;
-    num /= 10;
-    low = num%10;
+  if(num == 0)
+    numbuf[i++] = 48;
+  else{
+    while(num != 0)
+    {
+      numbuf[i++] = 48 + low;
+      num /= 10;
+      low = num%10;
+    }  
   }
+  numbuf[i] = '\0';
+  len = strlen(numbuf);
+  for(int j = len-1; j >= 0; j--)
+    out[nindex++] = numbuf[j];
   out[nindex] = '\0';
   return;
 }
 
 void u2a(uint32_t num,int nindex,char *out)
 {
+  int i = 0,len;
+  char numbuf[2000];
   uint32_t low = num%10;
-  while(low != 0)
-  {
-    out[nindex++] = 48 + low;
-    num/=10;
-    low = num%10;
+  if(num == 0)
+    numbuf[i++] = 48;
+  else{
+    while(num != 0)
+    {
+      numbuf[i++] = 48 + low;
+      num /= 10;
+      low = num%10;
+    }  
   }
+  numbuf[i] = '\0';
+  len = strlen(numbuf);
+  for(int j = len-1; j >= 0; j--)
+    out[nindex++] = numbuf[j];
   out[nindex] = '\0';
   return;
 }
@@ -41,56 +61,55 @@ int printf(const char *fmt, ...) {
 
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
+  if(out == NULL)
+    return -1;
   int len = strlen(fmt),index = 0;
   out[0] = '\0';
   for(int i = 0; i < len; i++) {
     if(fmt[i] == '%')
     {
-      char c = fmt[++i];
+      char c = fmt[i+1];
       switch(c)
       {
         case 's':{
           char *str = va_arg(ap,char *);
           strcat(out,str);
-          i++;//离开’ｓ‘
-          break;
-        }
-        case 'c':{
-          int num = va_arg(ap,int);
-          int nindex = strlen(out);
-          out[nindex++] = (char)num;
-          out[index] = '\0';
-          i++;
+          i+=2;//离开’ｓ‘
           break;
         }
         case 'd':{
           int num = va_arg(ap,int);
           int nindex = strlen(out);
           i2a(num,nindex,out);
-          i++;
+          i+=2;
           break;
         }
         case 'u':{
-          uint32_t num = va_arg(ap,uint32_t);
+          uint32_t num = (uint32_t)va_arg(ap,int);
           int nindex = strlen(out);
           u2a(num,nindex,out);
-          i++;
+          i+=2;
           break;
         }
         default:{
-          i++;
           break;
         }
       }
     }
     index = strlen(out);
+    if(i >= len) {
+      out[index] = '\0';
+      break;
+    }
     out[index++] = fmt[i];
     out[index] = '\0';
-  }
+  };
   return index;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
+  if(out == NULL)
+    return -1;
   va_list ap;
   int cnt;
   va_start(ap,fmt);
@@ -100,6 +119,8 @@ int sprintf(char *out, const char *fmt, ...) {
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
+  if(out == NULL)
+    return -1;
   va_list ap;
   char buf[65535];
   va_start(ap,fmt);
