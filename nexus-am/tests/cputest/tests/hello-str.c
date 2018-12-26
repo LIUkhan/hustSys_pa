9ecdfd84c206a78bfb401afe186b4da19a590a61
tracer-ics2018 <tracer@njuics.org>
2018-12-26 23:10:23 +0800

    >  run U201514639 khan Linux khan 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  23:10:23 up 15:10,  1 user,  load average: 0.23, 0.33, 0.43 48dd999bc09cccfb5bc7fb5a9a0b4c2e4bd7aea

--- nexus-am/tests/cputest/tests/hello-str.c
+++ nexus-am/tests/cputest/tests/hello-str.c
@@ -6,10 +6,10 @@ int main() {
 	sprintf(buf, "%s", "Hello world!\n");
 	nemu_assert(strcmp(buf, "Hello world!\n") == 0);
 
-	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
+	sprintf(buf, "%d + %8d = %d\n", 1, 1, 2);
 	nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
 
-	sprintf(buf, "%d + %d = %d\n", 2, 10, 12);
+	sprintf(buf, "%d + %01d = %d\n", 2, 10, 12);
 	nemu_assert(strcmp(buf, "2 + 10 = 12\n") == 0);
 
 	return 0;
