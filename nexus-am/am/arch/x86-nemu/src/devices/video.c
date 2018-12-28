be55bdc3ea3a83162aaedb4b3126bce6fdbff654
tracer-ics2018 <tracer@njuics.org>
2018-12-28 11:34:04 +0800

    >  run U201514639 khan Linux khan 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  11:34:04 up  3:28,  1 user,  load average: 1.51, 1.10, 0.78 afc62043a6aa741b40c6c37fbe08a2bbdd20a5c2

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -11,6 +11,9 @@
 static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
 
 
+static inline int min(int x, int y) {
+  return (x < y) ? x : y;
+}
 
 size_t video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
@@ -32,10 +35,20 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
       // int i;
       // int size =  screen_width() * screen_height();
       // for (i = 0; i < size; i ++) fb[i] = i;
-      int width = screen_width();
-      for(int y = ctl->y; y < ctl->y + ctl->h; y++)
-        for(int x = ctl->x; x < ctl->x + ctl->w; x++)
-          fb[y*width+x] = *(ctl->pixels);
+      // int width = screen_width();
+      // for(int y = ctl->y; y < ctl->y + ctl->h; y++)
+      //   for(int x = ctl->x; x < ctl->x + ctl->w; x++)
+      //     fb[y*width+x] = *(ctl->pixels);
+
+      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+      int W = screen_width();
+      int H = screen_height();
+      uint32_t *pixels = ctl->pixels;
+      int cp_bytes = sizeof(uint32_t) * min(w, W - x);
+      for (int j = 0; j < h && y + j < H; j ++) {
+        memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
+        pixels += w;
+      }
       if (ctl->sync) {
         // do nothing, hardware syncs.
       }
