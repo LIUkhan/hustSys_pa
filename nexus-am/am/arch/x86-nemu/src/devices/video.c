93d536a15ed3e1d5785a469061c73cb8b152ee34
tracer-ics2018 <tracer@njuics.org>
2018-12-28 11:11:50 +0800

    >  run U201514639 khan Linux khan 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  11:11:50 up  3:06,  1 user,  load average: 0.24, 0.58, 0.54 3e1ae2e8a7f5d10b3df9b75f333a1661d7b3bf1

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -5,8 +5,8 @@
 
 #define SCREEN_PORT 0x100
 
-#define W 400
-#define H 300
+// #define W 400
+// #define H 300
 
 static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
 
@@ -23,11 +23,11 @@ size_t video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _VideoInfoReg *info = (_VideoInfoReg *)buf;
-      // uint32_t WH = inl(SCREEN_PORT);//from vga.c
-      // info->width =  (WH >>16)/2;
-      // info->height = (WH && 0xffff)/2;
-      info->width = screen_width();
-      info->height = screen_height();
+      uint32_t WH = inl(SCREEN_PORT);//from vga.c
+      info->width =  WH >>16;
+      info->height = WH && 0xffff;
+      // info->width = screen_width();
+      // info->height = screen_height();
       return sizeof(_VideoInfoReg);
     }
   }
@@ -38,13 +38,13 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
-      // int i;
-      // int size =  * ;
-      // for (i = 0; i < size; i ++) fb[i] = i;
-      int width = screen_width();
-      for(int y = ctl->y; y < ctl->y + ctl->h; y++)
-        for(int x = ctl->x; x < ctl->x + ctl->w; x++)
-          fb[y*width+x] = *(ctl->pixels);
+      int i;
+      int size =  screen_width() * screen_height();
+      for (i = 0; i < size; i ++) fb[i] = i;
+      // int width = screen_width();
+      // for(int y = ctl->y; y < ctl->y + ctl->h; y++)
+      //   for(int x = ctl->x; x < ctl->x + ctl->w; x++)
+      //     fb[y*width+x] = *(ctl->pixels);
       if (ctl->sync) {
         // do nothing, hardware syncs.
       }
