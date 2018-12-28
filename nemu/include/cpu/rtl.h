b1e3b9c300689af4cdf02df2db0f323bc0b4dd17
tracer-ics2018 <tracer@njuics.org>
2018-12-28 23:45:01 +0800

    >  compile U201514639 khan Linux khan 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  23:45:01 up  7:47,  1 user,  load average: 0.41, 0.22, 0.09 9023323be02554063c21eaaf547805b883f5522d

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -161,7 +161,7 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   *dest = temp>>offset;
 }
 
-void rtl_push(const rtlreg_t* src1) {
+static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
   reg_l(R_ESP) -= 4;
