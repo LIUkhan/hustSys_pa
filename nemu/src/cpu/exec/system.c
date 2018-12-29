9b20e634918f81063d5314ea13f64cb6a5a8bbfd
tracer-ics2018 <tracer@njuics.org>
2018-12-29 15:54:05 +0800

    >  compile U201514639 khan Linux khan 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  15:54:05 up  7:38,  1 user,  load average: 0.41, 0.35, 0.29 68c5b948d39caa6a25e033f6ddf6078b41970c4

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -12,19 +12,24 @@ extern void raise_intr(uint8_t, vaddr_t);
 
 make_EHelper(lidt) {
   // TODO();
-  rtlreg_t low16,high32,mask,baseaddr;
-  rtl_li(&mask,0xffffff);
-  rtl_li(&baseaddr,id_dest->val+2);
-  rtl_lm(&low16,&(id_dest->val),2);
-  rtl_lm(&high32,&baseaddr,4);
-  rtl_mv(&(cpu.IDTR.limit),&low16);
+  // rtlreg_t low16,high32,mask,baseaddr;
+  // rtl_li(&mask,0xffffff);
+  // rtl_li(&baseaddr,id_dest->val+2);
+  // rtl_lm(&low16,&(id_dest->val),2);
+  // rtl_lm(&high32,&baseaddr,4);
+  // rtl_mv(&(cpu.IDTR.limit),&low16);
   
-  if(decoding.is_operand_size_16)   
-    rtl_and(&high32,&high32,&mask);
+  // if(decoding.is_operand_size_16)   
+  //   rtl_and(&high32,&high32,&mask);
   
-  rtl_mv(&(cpu.IDTR.base),&high32);
+  // rtl_mv(&(cpu.IDTR.base),&high32);
+  rtlreg_t baseaddr;
+  rtl_lm(&cpu.IDTR.limit, &(id_dest->val), 2);
+  rtl_addi(&baseaddr, &id_dest->addr, 2);
+  rtl_lm(&cpu.IDTR.base, &baseaddr, 4);  
   print_asm_template1(lidt);
 }
+
 
 make_EHelper(mov_r2cr) {
   TODO();
