ea20468ca466289f8effb191d3389e9d02995c2a
tracer-ics2018 <tracer@njuics.org>
2019-01-01 18:00:50 +0800

    >  run U201514639 khan Linux khan 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  18:00:50 up 1 day,  7:37,  1 user,  load average: 0.54, 0.27, 0.29 35b6296d558552ec91b0d2303f0a1334243103a7

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,8 +4,15 @@
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  printf("0x%x\n",a[0]);
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
+  printf("0x%x 0x%x 0x%x 0x%x\n",a[0],a[1],a[2],a[3]);
   switch (a[0]) {
+    //  case SYS_exit: {
+    //   // _halt();
+    //   break;
+    // }
     case SYS_yield: {
       _yield();
       c->GPR1 = 0;
