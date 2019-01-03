7c22432ccf913f660b4586359fdadd95ae3c6624
tracer-ics2018 <tracer@njuics.org>
2019-01-03 14:05:44 +0800

    >  run U201514639 khan Linux khan 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  14:05:44 up  6:04,  1 user,  load average: 0.67, 0.37, 0.25 420cd8b6dbd8dd808e6238842aca9befd0d19ec

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -34,8 +34,8 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   offset /= 4;
-  int x = offset % screen_height();
-  int y = offset / screen_width();
+  int x = offset / screen_height();
+  int y = offset % screen_width();
   draw_rect((uint32_t *)buf,x,y,len/4,1);
   return len;
 }
