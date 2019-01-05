f41d260b55198f9fdc1fb17822dc048928e273d8
tracer-ics2018 <tracer@njuics.org>
2019-01-05 20:36:25 +0800

    >  run U201514639 khan Linux khan 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  20:36:25 up 10:30,  1 user,  load average: 0.19, 0.19, 0.12 2b7accb0e77c40e737a92d7fee9c5cc93b0fc33e

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -73,11 +73,11 @@ int _cte_init(_Context*(*handler)(_Event, _Context*)) {
 // } _Area; 
 _Context *_kcontext(_Area stack, void (*entry)(void *), void *arg) {
   void *base = stack.end;
-  _Context * nc = (_Context *)base;
+  _Context * nc = (_Context *)base + sizeof(_Context);
   memset(nc, 0, sizeof(_Context));
-  // nc->eip = (uint32_t)entry;
+  nc->eip = (uint32_t)entry;
   // printf("0x%x\n",nc->eip);
-  // nc->cs = 0x8;
+  nc->cs = 0x8;
   uintptr_t *tf = (uintptr_t *)stack.start;
   *tf = (uintptr_t)nc;  
   return nc;
