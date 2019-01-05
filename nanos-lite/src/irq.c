2199ae8ac0dade64c5e4d20ce6bd0341ca0d1958
tracer-ics2018 <tracer@njuics.org>
2019-01-05 19:31:04 +0800

    >  compile U201514639 khan Linux khan 4.4.0-141-generic #167-Ubuntu SMP Wed Dec 5 10:40:15 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  19:31:04 up  9:25,  1 user,  load average: 0.87, 0.35, 0.19 5c5650f7e31c9e0b4f796fc7c6a6f1349aa60415

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,12 +5,12 @@ extern _Context* schedule(_Context *);
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
     case 5:{
-      // printf("_event_yield\n");
+      printf("_event_yield\n");
       c = schedule(c);
       break;
     }
     case 6:{
-      // printf("_event_syscall\n");
+      printf("_event_syscall\n");
       c = do_syscall(c);
       break;
     }
